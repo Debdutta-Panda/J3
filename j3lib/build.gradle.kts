997f96b7d6480a0686e8f6c3b63f35af55f03e3c
deb <me@debduttapanda.com>
2023-11-08 01:25:14 +0530

    1.1.20

--- j3lib/build.gradle.kts
+++ j3lib/build.gradle.kts
@@ -74,7 +74,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.debduttapanda"
             artifactId = "j3"
-            version = "1.1.18"
+            version = "1.1.20"
 
             afterEvaluate {
                 from(components["release"])
