69cff66b7798477c290cb731b90f7a8fdf2db88b
Debdutta Panda <me@debduttapanda.com>
2023-11-17 14:48:40 +0530

    1.1.29

--- j3lib/build.gradle.kts
+++ j3lib/build.gradle.kts
@@ -75,7 +75,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.debduttapanda"
             artifactId = "j3"
-            version = "1.1.28"
+            version = "1.1.29"
 
             afterEvaluate {
                 from(components["release"])
